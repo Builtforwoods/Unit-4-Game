00b14082c80046e2e6e2e89a718f98fe7eb2aae0
Builtforwoods <rodolfomlphx@gmail.com>
2019-10-05 23:53:45 -0700

    made the javascript work, made vars, incrementers and appended them to their respective html divs

---
+++ Assets/Javascript/game.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+  let firstNumber = Math.floor(Math.random() * Math.floor(100));
+  let playerScore = 0;
+  let wins;
+  let losses;
+
+  $("#greenBox").append("<h1>" + firstNumber + "</h1>");
+
+  $("#redRuby").on("click", function() {
+    if (playerScore > firstNumber) {
+      alert("You lose!");
+      playerScore = 0;
+      losses++;
+      $("#lossehs").html("<p> Losses: " + losses + "</p>");
+    } else if (playerScore === firstNumber) {
+      alert("You win!");
+      playerScore = 0;
+      wins++;
+      $("#weens").html("<p> Wins: " + wins + "</p>");
+    } else {
+      playerScore += 5;
+      $("#scoreCount").html("<h1>" + playerScore + "</h1>");
+    }
+  });
+
+  $("#blueDiamond").on("click", function() {
+    if (playerScore > firstNumber) {
+      alert("You lose!");
+      playerScore = 0;
+      losses++;
+      $("#lossehs").html("<p> Losses: " + losses + "</p>");
+    } else if (playerScore === firstNumber) {
+      alert("You win!");
+      playerScore = 0;
+      wins++;
+      $("#weens").html("<p> Wins: " + wins + "</p>");
+    } else {
+      playerScore += 3;
+      $("#scoreCount").html("<h1>" + playerScore + "</h1>");
+    }
+  });
+
+  $("#yellowGem").on("click", function() {
+    if (playerScore > firstNumber) {
+      alert("You lose!");
+      playerScore = 0;
+      losses++;
+      $("#lossehs").html("<p> Losses: " + losses + "</p>");
+    } else if (playerScore === firstNumber) {
+      alert("You win!");
+      playerScore = 0;
+      wins++;
+      $("#weens").html("<p> Wins: " + wins + "</p>");
+    } else {
+      playerScore += 2;
+      $("#scoreCount").html("<h1>" + playerScore + "</h1>");
+    }
+  });
+  $("#greenGem").on("click", function() {
+    if (playerScore > firstNumber) {
+      alert("You lose!");
+      playerScore = 0;
+      losses++;
+      $("#lossehs").html("<p> Losses: " + losses + "</p>");
+    } else if (playerScore === firstNumber) {
+      alert("You win!");
+      playerScore = 0;
+      wins++;
+      $("#weens").html("<p> Wins: " + wins + "</p>");
+    } else {
+      playerScore += 7;
+      $("#scoreCount").html("<h1>" + playerScore + "</h1>");
+    }
+  });
+});
